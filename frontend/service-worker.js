5f8d26c72353839f2a46deb9dec73b2316f0b99f
paopao0380-svg <paopao0380@gmail.com>
2026-08-12 11:22:39 -0500

    Add files via upload

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,5 +1,5 @@
 // Enlace Escolar SW
-const CACHE_NAME = 'enlace-escolar-v20260812g';
+const CACHE_NAME = 'enlace-escolar-v20260812h';
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(['./index.html','./manifest.json']).catch(()=>{})));
